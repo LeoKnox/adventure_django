820bb0c521a9fbcf70686a3e90393f928a9159ad
LeoKnox <LeoKnox@users.noreply.github.com>
2021-07-08 19:52:15 -0700

    Add files via upload

--- adventure/settings.py
+++ adventure/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNuberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Application definition
 
