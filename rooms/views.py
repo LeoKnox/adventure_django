bf2c158c0ca206b79fae9e8f7dbe67eec11e9127
LeoKnox <LeoKnox@users.noreply.github.com>
2021-07-15 20:25:37 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -123,7 +123,7 @@ def treasure(request):
         new_treasure.name = request.POST.get('treasure_name'),
         new_treasure.description = request.POST.get('treasure_description'),
         new_treasure.room_id = request.POST.get('treasure_rooms')
-        new_treasure.save()
+        new_treasure.create()
     return render(request, 'treasure.html', {'room_treasure': room_treasure, 'rooms': rooms})
 
 def delete_treasure(request, treasure_id):
