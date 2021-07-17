d76145bb0554e6496dc0151aa0e15b546afc2684
LeoKnox <LeoKnox@users.noreply.github.com>
2021-07-16 21:15:35 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -132,7 +132,7 @@ def delete_treasure(request, treasure_id):
 
 def edit_treasure(request, treasure_id):
     edit_treasure = Treasure.objects.get(pk = treasure_id)
-    room_treasures = Room.objects.get(pk = treasure_id)
+    room_treasures = treasure.room_set.get(pk = treasure_id)
     if request.method == "POST":
         if request.POST.get('name') != "":
             edit_treasure.name = request.POST.get('name')
