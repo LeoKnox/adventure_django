87770524146adc7a96ca64bcff12e76eaea44e37
LeoKnox <LeoKnox@users.noreply.github.com>
2021-08-04 20:34:33 -0700

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -23,7 +23,8 @@ class DoorManager(models.Manager):
         #if int(postData['location']) >= Room.objects.get(id=postData['id']).location:
             #errors['maximum_location'] = "Door max exceeded" + str(Door.objects.filter(location=postData['location']).first().location)
         #if int(postData['location']) > Door.objects.get(id=postData['id']).location:
-        if int(postData['location']) > Room.objects.get(id=postData['room_id']).width:
+        room_validator = Room.objects.get(id=postData['room_id'])
+        if int(postData['location']) > room_validator.width:
             print("error error err... ")
             errors['maximum_location'] = "Door cannot exceed length " + str(Room.objects.get(id=postData['room_id']).width)
         return errors
