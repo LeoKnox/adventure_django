83ebc5dd6c70be0612e212d41e6f1e3f6296f990
LeoKnox <LeoKnox@users.noreply.github.com>
2021-08-30 19:10:22 -0700

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -38,7 +38,7 @@ class RoomManager(models.Manager):
 class DoorManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
-        if postData['wall'].isnumeric():
+        if postData['wall'].isnumeric() != True:
             print('Wonder Wall! ********')
         if len(postData.get('next_room')) < 2:
             errors['next_room'] = "Door name should be more then 2 characters"
