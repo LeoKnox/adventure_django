ab36a412c97176a207e6d523a39091a1ceb1d00d
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-04 21:34:59 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -156,7 +156,8 @@ def edit_treasure(request, treasure_id):
     #treasure_rooms = Treasure.objects.filter(name=edit_treasure.name)
     #treasure_rooms = Treasure.objects.get(pk = treasure_id)
     #treasure_rooms.room._meta.get_field('name').remote_field.model.__name__
-    room_treasures = Room.objects.filter(name = edit_treasure.name)
+    #room_treasures = Room.objects.filter(name = edit_treasure.name)
+    room_treasures = Treasure.objects.filter(name = edit_treasure.name)
     print("====")
     print(room_treasures)
     if request.method == "POST":
